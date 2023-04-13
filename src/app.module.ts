d85ed5b8941b03fa5f1f388b24e8fec7f30efcfc
phuochungus <21520252@gm.uit.edu.vn>
2023-04-13 20:15:35 +0700

    update .gitignore skip .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VouchersModule } from './vouchers/vouchers.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { config } from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     VouchersModule,
     MongooseModule.forRoot(
       'mongodb+srv://dev:' +
