d2ed3b5229f67a2ef9f4c582509c2327bb79af74
phuochungus <21520252@gm.uit.edu.vn>
2023-04-14 18:14:12 +0700

    define size properties

--- src/sizes/entities/size.entity.ts
+++ src/sizes/entities/size.entity.ts
@@ -1,1 +1,18 @@
-export class Size {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class Size {
+  @Prop({ required: true })
+  widthInCentimeter: number;
+
+  @Prop({ required: true })
+  heightInCentimeter: number;
+
+  @Prop({ required: true, default: null })
+  depthInCentimeter: number | null;
+
+  @Prop({ required: true })
+  lable: string;
+}
+
+export const SizeSchema = SchemaFactory.createForClass(Size);
