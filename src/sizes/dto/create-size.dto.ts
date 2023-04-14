e8970ac0b7f584d1e27e18f34b40d8caeb1a7d99
phuochungus <21520252@gm.uit.edu.vn>
2023-04-14 18:17:16 +0700

    define create size dto

--- src/sizes/dto/create-size.dto.ts
+++ src/sizes/dto/create-size.dto.ts
@@ -1,1 +1,17 @@
-export class CreateSizeDto {}
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { Size } from '../entities/size.entity';
+
+export class CreateSizeDto implements Size {
+  @IsNumber()
+  widthInCentimeter: number;
+
+  @IsNumber()
+  heightInCentimeter: number;
+
+  @IsOptional()
+  @IsNumber()
+  depthInCentimeter: number | null;
+
+  @IsString()
+  lable: string;
+}
