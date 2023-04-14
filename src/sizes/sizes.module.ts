c477b89a1ce80265fdf0e9ea50669ddb6ce1e73f
phuochungus <21520252@gm.uit.edu.vn>
2023-04-14 18:18:49 +0700

    import mongoose module

--- src/sizes/sizes.module.ts
+++ src/sizes/sizes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SizesService } from './sizes.service';
 import { SizesController } from './sizes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Size } from './entities/size.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Size.name, schema: Size }])],
   controllers: [SizesController],
-  providers: [SizesService]
+  providers: [SizesService],
 })
 export class SizesModule {}
