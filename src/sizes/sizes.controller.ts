5c98addf94a66bc9276d45383c6bee8a081aee60
phuochungus <21520252@gm.uit.edu.vn>
2023-04-14 18:40:18 +0700

    remove unuse method

--- src/sizes/sizes.controller.ts
+++ src/sizes/sizes.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { SizesService } from './sizes.service';
 import { CreateSizeDto } from './dto/create-size.dto';
-import { UpdateSizeDto } from './dto/update-size.dto';
 
 @Controller('sizes')
 export class SizesController {
@@ -15,20 +19,5 @@ export class SizesController {
   @Get()
   findAll() {
     return this.sizesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.sizesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSizeDto: UpdateSizeDto) {
-    return this.sizesService.update(+id, updateSizeDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.sizesService.remove(+id);
   }
 }
