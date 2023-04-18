40a6e59da9851ba34e16507ad924d88b1c290757
phuochungus <21520252@gm.uit.edu.vn>
2023-04-18 14:15:22 +0700

    add paging and remove redundant field

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Product } from './schemas/product.schema';
 import { Model } from 'mongoose';
+import QueryProductDTO from './dto/query-prodict.dto';
 
 @Injectable()
 export class ProductsService {
@@ -47,7 +48,6 @@ export class ProductsService {
     return await this.productModel
       .findOne({ _id: objectId })
       .select(['-createdAt', '-updatedAt'])
-      .sort({ createdAt: -1 })
       .populate({
         path: 'colorIds',
         select: {
@@ -75,37 +75,25 @@ export class ProductsService {
     if (!product) throw new NotFoundException();
   }
 
-  async findBestSellers() {
+  async findBestSellers(queryProductDto: QueryProductDTO) {
     return await this.productModel
       .find()
-      .select(['-createdAt', '-updatedAt'])
-      .sort({ createdAt: -1 })
-      .limit(15)
-      .populate({
-        path: 'colorIds',
-        select: {
-          _id: 0,
-          name: 1,
-          hex: 1,
-        },
-      })
-      .populate({
-        path: 'sizeIds',
-        select: {
-          _id: 0,
-          widthInCentimeter: 1,
-          heightInCentimeter: 1,
-          lable: 1,
-        },
-      });
+      .select(['_id', 'name', 'imageURL', 'price', 'saleOffPrice', 'sold'])
+      .sort({ sold: -1 })
+      .skip(15 * queryProductDto.page)
+      .limit(15);
   }
 
-  async findBestSaleOff() {
+  async findBestSaleOff(queryProductDto: QueryProductDTO) {
     return await this.productModel.aggregate([
       {
         $project: {
-          createdAt: 0,
-          updatedAt: 0,
+          _id: 1,
+          name: 1,
+          rating: 1,
+          imageURL: 1,
+          price: 1,
+          saleOffPrice: 1,
         },
       },
       {
@@ -115,47 +103,53 @@ export class ProductsService {
           },
         },
       },
+      {
+        $sort: { pecentSaleOff: -1 },
+      },
+      {
+        $skip: 15 * queryProductDto.page,
+      },
       { $limit: 15 },
-      {
-        $lookup: {
-          from: 'colors',
-          localField: 'colorIds',
-          foreignField: '_id',
-          pipeline: [
-            {
-              $project: {
-                _id: 0,
-                createdAt: 0,
-                updatedAt: 0,
-              },
-            },
-          ],
-          as: 'colors',
-        },
-      },
-      {
-        $lookup: {
-          from: 'sizes',
-          localField: 'sizeIds',
-          foreignField: '_id',
-          pipeline: [
-            {
-              $project: {
-                _id: 0,
-                createdAt: 0,
-                updatedAt: 0,
-              },
-            },
-          ],
-          as: 'sizes',
-        },
-      },
-      {
-        $project: {
-          colorIds: 0,
-          sizeIds: 0,
-        },
-      },
+      // {
+      //   $lookup: {
+      //     from: 'colors',
+      //     localField: 'colorIds',
+      //     foreignField: '_id',
+      //     pipeline: [
+      //       {
+      //         $project: {
+      //           _id: 0,
+      //           createdAt: 0,
+      //           updatedAt: 0,
+      //         },
+      //       },
+      //     ],
+      //     as: 'colors',
+      //   },
+      // },
+      // {
+      //   $lookup: {
+      //     from: 'sizes',
+      //     localField: 'sizeIds',
+      //     foreignField: '_id',
+      //     pipeline: [
+      //       {
+      //         $project: {
+      //           _id: 0,
+      //           createdAt: 0,
+      //           updatedAt: 0,
+      //         },
+      //       },
+      //     ],
+      //     as: 'sizes',
+      //   },
+      // },
+      // {
+      //   $project: {
+      //     colorIds: 0,
+      //     sizeIds: 0,
+      //   },
+      // },
     ]);
   }
 }
