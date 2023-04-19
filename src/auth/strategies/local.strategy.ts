be6cfa42fa36e4cdf09aa513847c9a03535146a4
phuochungus <21520252@gm.uit.edu.vn>
2023-04-19 17:05:01 +0700

    implement local strategy and export local guard

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export default class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+  constructor(private usersService: UsersService) {
+    super();
+  }
+
+  async validate(username: string, password: string) {
+    const payload =
+      await this.usersService.findUserMatchUsernameAndPasswordOrFail(
+        username,
+        password,
+      );
+    return payload;
+  }
+}
