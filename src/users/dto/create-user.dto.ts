3ad12a5dce5a61f4f19119b21874f7a0286eb248
phuochungus <21520252@gm.uit.edu.vn>
2023-04-19 10:42:23 +0700

    add create-user dto and user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,43 @@
-export class CreateUserDto {}
+import { gender } from '../schemas/user.schema';
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsMobilePhone,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsOptional()
+  @IsString()
+  fullname: string;
+
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsMobilePhone('vi-VN')
+  phone: string;
+
+  @IsOptional()
+  @IsEmail()
+  email: string | undefined;
+
+  @IsOptional()
+  @IsEnum(gender)
+  gender: gender | undefined;
+
+  @IsOptional()
+  @IsDateString()
+  @Type(() => Date)
+  birth: Date;
+
+  @IsOptional()
+  @IsString()
+  address: string;
+}
