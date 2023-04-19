b6b92bdbda0456535766c7b060d69c014034cf0a
phuochungus <21520252@gm.uit.edu.vn>
2023-04-19 17:01:35 +0700

    change name validation pipe

--- src/pipes/validate-mongoId.pipe.ts
+++ src/pipes/validate-mongoId.pipe.ts
@@ -4,8 +4,10 @@ import { ArgumentMetadata, PipeTransform } from '@nestjs/common/interfaces';
 import { isValidObjectId } from 'mongoose';
 
 @Injectable()
-export default class ParseObjectIdStringPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+export default class ValidateMongoIdPipe
+  implements PipeTransform<string, string>
+{
+  transform(value: string, metadata: ArgumentMetadata): string {
     if (isValidObjectId(value)) return value;
     throw new BadRequestException(
       'Validation failed (objectID string is expected)',
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,12 +5,14 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import ParseObjectIdStringPipe from 'src/pipes/parse-objectID-string.pipe';
+import JWTAuthGuard from 'src/auth/guards/jwt-auth.guard';
+import ValidateMongoIdPipe from 'src/pipes/validate-mongoId.pipe';
+import { User } from './schemas/user.schema';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -22,7 +24,14 @@ export class UsersController {
   }
 
   @Get('/user/:id')
-  findOne(@Param('id', ParseObjectIdStringPipe) id: string) {
-    return this.usersService.findOne(id);
+  findOne(@Param('id', ValidateMongoIdPipe) id: string) {
+    return this.usersService.findOneOrFail(id);
+  }
+
+  @Patch()
+  @UseGuards(JWTAuthGuard)
+  // @UseInterceptors(UserInterceptor)
+  update(@CurrentUser() user: User) {
+    return user;
   }
 }
