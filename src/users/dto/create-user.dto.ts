90dc2a1671f19e212cabeaee998f7ee7ba95785a
phuochungus <21520252@gm.uit.edu.vn>
2023-04-19 17:02:31 +0700

    rewrite dto class to match app design

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,43 +1,9 @@
-import { gender } from '../schemas/user.schema';
-import {
-  IsDateString,
-  IsEmail,
-  IsEnum,
-  IsMobilePhone,
-  IsOptional,
-  IsString,
-} from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsString, IsMongoId } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  username: string;
-
-  @IsOptional()
-  @IsString()
-  fullname: string;
+  usernameOrEmail: string;
 
   @IsString()
   password: string;
-
-  @IsOptional()
-  @IsMobilePhone('vi-VN')
-  phone: string;
-
-  @IsOptional()
-  @IsEmail()
-  email: string | undefined;
-
-  @IsOptional()
-  @IsEnum(gender)
-  gender: gender | undefined;
-
-  @IsOptional()
-  @IsDateString()
-  @Type(() => Date)
-  birth: Date;
-
-  @IsOptional()
-  @IsString()
-  address: string;
 }
