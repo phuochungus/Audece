047fe16c211885f11397f5f61da4bc3b56692424
phuochungus <21520252@gm.uit.edu.vn>
2023-04-20 19:52:01 +0700

    nothing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -29,7 +29,7 @@ export class AuthController {
     return this.authService.createAccountOrGenerateAccessTokenIfExist(user);
   }
 
-  @Get('facebook')
+  @Get('/facebook')
   @UseGuards(FacebookAuthGuard)
   loginFacebook() {}
 
