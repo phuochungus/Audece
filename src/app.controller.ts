2d3ef1749ca717a8eb210a3adf59d2ee099bfa0b
phuochungus <21520252@gm.uit.edu.vn>
2023-04-20 19:27:07 +0700

    test guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,8 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import LocalAuthGuard from './auth/guards/local-auth.guard';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import JWTAuthGuard from './auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private authService: AuthService,
-  ) {}
-
-  @Post()
-  @UseGuards(LocalAuthGuard)
-  login(@Request() req) {
-    return {
-      accessToken: this.authService.generateAccessTokenString(req.user._id),
-    };
-  }
-
   @Get()
   @UseGuards(JWTAuthGuard)
   protected(@Request() req) {
