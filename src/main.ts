51c8864392db5d20fbbf060351ffe32715fa3d8b
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-07-18 09:23:13 +0700

    use compression

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
